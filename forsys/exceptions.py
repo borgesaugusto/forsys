d3c187e89a653be1504da56f7e549c494ec85db5
borgesaugusto <borges.augustoar@gmail.com>
2022-10-26 10:20:42 +0200

    Added SegmentationArtifactException to detect non-confluent tissue

--- forsys/exceptions.py
+++ forsys/exceptions.py
@@ -1,3 +1,7 @@
 class DifferentTissueException(Exception):
     def __init__(self, message='Tissues are too different'):
         super(DifferentTissueException, self).__init__(message)
+
+class SegmentationArtifactException(Exception):
+    def __init__(self, message='There might be a segmentation artifact from the TIFF'):
+        super(SegmentationArtifactException, self).__init__(message)
