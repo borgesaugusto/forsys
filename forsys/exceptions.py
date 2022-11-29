91846267b9204482e6a57e6d635525fb786358ce
borgesaugusto <borges.augustoar@gmail.com>
2022-11-29 17:18:51 +0100

    Changed exit() for an appropiate exception

--- forsys/exceptions.py
+++ forsys/exceptions.py
@@ -5,3 +5,7 @@ class DifferentTissueException(Exception):
 class SegmentationArtifactException(Exception):
     def __init__(self, message='There might be a segmentation artifact from the TIFF'):
         super(SegmentationArtifactException, self).__init__(message)
+
+class BigEdgesBadlyCreated(Exception):
+    def __init__(self, message='Big edge was not created correctly'):
+        super(BigEdgesBadlyCreated, self).__init__(message)
