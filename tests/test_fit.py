721fa921d4db1705449ed137628308d24c5a8647
Augusto Borges <borges.augustoar@gmail.com>
2024-01-05 17:53:49 +0100

    Change test of LSQ to not have the mean = 1 condition

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -66,8 +66,7 @@ def test_fit_furrow_velocity_lsq_initial_last_val(furrow):
     all_r_values = []
     for ii in furrow.frames.keys():
         if ii == 0:
-            initial_condition = np.ones(len(furrow.frames[0].internal_big_edges) + 1)
-            initial_condition[-1] = 0
+            initial_condition = np.ones(len(furrow.frames[0].internal_big_edges))
         else:
             initial_condition = furrow.frames[ii-1].get_tensions()["stress"]
 
@@ -78,7 +77,7 @@ def test_fit_furrow_velocity_lsq_initial_last_val(furrow):
                             method="lsq")
         tensions_df = furrow.frames[ii].get_tensions()
         r_value = distance_to_yeqx(tensions_df['gt'].values / tensions_df['gt'].mean(), 
-                                    tensions_df['stress'].values)
+                                    tensions_df['stress'].values / tensions_df['stress'].mean())
         all_r_values.append(r_value)
     print("Dynamic furrow using last", all_r_values)
     assert np.all([1 > value > 0.92 for value in all_r_values])
@@ -87,7 +86,6 @@ def test_fit_furrow_velocity_lsq_initial_gt(furrow):
     all_r_values = []
     for ii in furrow.frames.keys():
         initial_condition = list(furrow.frames[ii].get_gt_tensions()["gt"].values)
-        initial_condition.append(0)
 
         furrow.build_force_matrix(when=ii)
         furrow.solve_stress(when=ii,
@@ -96,7 +94,7 @@ def test_fit_furrow_velocity_lsq_initial_gt(furrow):
                             method="lsq")
         tensions_df = furrow.frames[ii].get_tensions()
         r_value = distance_to_yeqx(tensions_df['gt'].values / tensions_df['gt'].mean(), 
-                                    tensions_df['stress'].values)
+                                    tensions_df['stress'].values / tensions_df['stress'].mean())
         all_r_values.append(r_value)
     print("Dynamic furrow using GT", all_r_values)
     assert np.all([1 > value > 0.94 for value in all_r_values])
