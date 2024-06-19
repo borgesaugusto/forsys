0645d9de72ff825ec5cdf1160a3e51477618fac1
nicolasaldecoa <nicolas.aldecoa@entreconsultas.com>
2024-06-19 20:29:04 -0300

    [ready for review] fix bugs due to rhs_matrix_ls being a vector

--- forsys/general_matrix.py
+++ forsys/general_matrix.py
@@ -41,13 +41,13 @@ class GeneralMatrix:
         assert self.rhs_matrix is not None, "RHS matrix not set"
 
         lhs_matrix_ls = self.lhs_matrix.T @ self.lhs_matrix
-        rhs_matrix_ls = self.lhs_matrix.T @ self.rhs_matrix
+        rhs_matrix_ls = self.lhs_matrix.T @ self.rhs_matrix  # vector
 
         removed_index = None
         if kwargs.get("method") == "fix_stress":
             lhs_matrix_ls, rhs_matrix_ls, removed_index = self.fix_one_stress(lhs_matrix_ls, rhs_matrix_ls)
         elif kwargs.get("method") == "lagrange_pressure":
-            lhs_matrix_ls, rhs_matrix_ls = self.add_lagrange_multiplier(lhs_matrix_ls, rhs_matrix_ls, 0)
+            lhs_matrix_ls, rhs_matrix_ls = self.add_lagrange_multiplier(lhs_matrix_ls, rhs_matrix_ls, 0.)
         
         lhs_matrix_ls = lhs_matrix_ls.astype(np.float64)
         rhs_matrix_ls = rhs_matrix_ls.astype(np.float64)
@@ -60,9 +60,8 @@ class GeneralMatrix:
                 xres, _ = scop.nnls(lhs_matrix_ls, rhs_matrix_ls, maxiter=kwargs.get("nnls_max_iter"))
 
         except np.linalg.LinAlgError:
-            # then try with nnls
             print("Numerically solving due to singular matrix")
-            xres, _ = scop.nnls(self.lhs_matrix, self.rhs_matrix, maxiter=kwargs.get("nnls_max_iter"))
+            xres, _ = scop.nnls(lhs_matrix_ls, rhs_matrix_ls, maxiter=kwargs.get("nnls_max_iter"))
 
         if removed_index is not None:
             xres = np.insert(xres, removed_index, 1.)
@@ -77,7 +76,7 @@ class GeneralMatrix:
     def add_lagrange_multiplier(self,
                                 lhs_matrix_ls: np.ndarray,
                                 rhs_matrix_ls: np.ndarray,
-                                constraint: Optional[float] = None
+                                constraint: float = 0.
                                 ) -> Tuple:
         """Add a constraint to the matrix system through a Lagrange multiplier. 
         LHS and RHS matrices should be already in least-squares form.
@@ -98,9 +97,7 @@ class GeneralMatrix:
         cMatrix = np.array([1.] * lhs_rows + [0.])
         lhs_matrix_ls = np.hstack((lhs_matrix_ls, cMatrix.reshape(-1, 1)))
 
-        rhs_matrix_ls = np.vstack((rhs_matrix_ls, np.zeros(rhs_matrix_ls.shape[1])))
-        if constraint is not None:
-            rhs_matrix_ls[-1, -1] = constraint
+        rhs_matrix_ls = np.concatenate((rhs_matrix_ls, constraint))
 
         return lhs_matrix_ls, rhs_matrix_ls
 
